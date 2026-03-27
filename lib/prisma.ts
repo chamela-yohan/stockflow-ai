52ccb9c572d911c649a76ac47ab44de49d6072df
ChamelaYohan <chamelayohan@gmail.com>
2026-03-27 21:05:55 +0530

    fix: prisma config (prisma.ts, schema.prisma)

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,15 +1,16 @@
-import { PrismaClient } from "../app/generated/prisma/client"; 
-import { PrismaPg } from "@prisma/adapter-pg"; 
+import { PrismaClient } from "../app/generated/prisma/client";
+
 const globalForPrisma = global as unknown as {
-  prisma: PrismaClient; 
-}; 
-const adapter = new PrismaPg({
-  connectionString: process.env.DATABASE_URL, 
-}); 
+  prisma: PrismaClient;
+};
+
 const prisma =
   globalForPrisma.prisma ||
   new PrismaClient({
-    adapter, 
-  }); 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma; 
+    accelerateUrl: process.env.DATABASE_URL,
+    log: ["warn", "error"],
+  });
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+
 export default prisma; 
